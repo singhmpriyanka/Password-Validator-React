5183d9d33b4789018c5ca292a35c75baea5902cd
Rajat Singh <rajatsingh167@gmail.com>
2024-12-11 19:01:39 +0530

    My password validator app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import validator from 'validator';
 
 function App() {
+  const [errMsg, seterrMsg] = useState('')
+  const [borderColor, setBorderColor] = useState('');
+  function validate(val) {
+    if (validator.isStrongPassword(val, {
+      minUppercase: 1, minLowercase: 1, minSymbols: 1, minLength: 5, minNumbers: 1
+    })) {
+      seterrMsg('Is strong password')
+      setBorderColor('green');
+    } else {
+      seterrMsg('Is not a strong password')
+      setBorderColor('red');
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className='App'>Password Validator</div>
+      <p className='Para'>
+        <h>Enter password: </h>
+        <input type='text' onChange={(e) => validate(e.target.value)} style={{ borderColor: borderColor, borderWidth: '4px', padding: '10px' }}></input>
+        <br />
+        <span style={{ color: borderColor }}>{errMsg}</span>
+      </p>
+    </>
+  )
 }
 
 export default App;
